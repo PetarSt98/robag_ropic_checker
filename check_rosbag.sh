6a9fa706253aa0d31ac60359a7f5fd51744e7707
Petar Stojkovic <petar.s@wingfin.com>
2023-07-28 01:16:36 +0200

    Update it to work in real time

--- check_rosbag.sh
+++ check_rosbag.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+
+POSITIONAL_ARGS=()
+SHOULD_BUILD_IMAGE=0
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --build)
+      SHOULD_BUILD_IMAGE=1
+      shift
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 if ! command -v xvfb &> /dev/null
@@ -10,7 +28,7 @@ else
 fi
 
 # Check if Docker image exists
-if [[ "$(docker images -q myimage 2> /dev/null)" == "" ]]; then
+if [[ "$(docker images -q myimage 2> /dev/null)" == "" || ${SHOULD_BUILD_IMAGE} = 1 ]]; then
     echo "Docker image doesn't exist. Building now..."
     docker build -t myimage $DIR
 else
@@ -19,7 +37,7 @@ fi
 
 xhost +
 
-docker run -it --rm \
+docker run -it --rm --net=host \
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     myimage
--- topic_checker.py
+++ topic_checker.py
@@ -3,53 +3,87 @@
 import rospy
 import Tkinter as tk
 
-# Define the topic groups
-sbg_topics = ['/sbg/ekf_euler', '/sbg/ekf_nav', '/sbg/ekf_quat', '/sbg/gps_pos', '/sbg/gps_raw', '/sbg/gps_vel', '/sbg/imu_data', '/sbg/status', '/sbg/utc_time']
-imu_topics = ['/imu/data', '/imu/nav_sat_fix', '/imu/pos_ecef', '/imu/temp', '/imu/utc_ref', '/imu/velocity']
-camera0_topics = ['/camera', '/camera_array/cam0/camera_info', '/camera_array/cam0/image_raw']
-camera1_topics = ['/camera_array/cam1/camera_info', '/camera_array/cam1/image_raw']
-camera2_topics = ['/camera_array/cam2/camera_info', '/camera_array/cam2/image_raw']
-steer_angle_topic = ['/steer_angle']
-wheelspeed_b_topic = ['/wheelspeed_b']
-wheelspeed_f_topic = ['/wheelspeed_f']
-velodyne_topics = ['/velodyne_nodelet_manager_driver/parameter_descriptions', '/velodyne_nodelet_manager_driver/parameter_updates', '/velodyne_packets']
-points_raw_topic = ['/points_raw']
-
+topic_groups = {
+    "sbg_topics": ['/sbg/ekf_euler', '/sbg/ekf_nav', '/sbg/ekf_quat', '/sbg/gps_pos', '/sbg/gps_raw', '/sbg/gps_vel', '/sbg/imu_data', '/sbg/status', '/sbg/utc_time'],
+    "imu_topics": ['/imu/data', '/imu/nav_sat_fix', '/imu/pos_ecef', '/imu/temp', '/imu/utc_ref', '/imu/velocity'],
+    "camera0_topics": ['/camera', '/camera_array/cam0/camera_info', '/camera_array/cam0/image_raw'],
+    "camera1_topics": ['/camera_array/cam1/camera_info', '/camera_array/cam1/image_raw'],
+    "camera2_topics": ['/camera_array/cam2/camera_info', '/camera_array/cam2/image_raw'],
+    "steer_angle_topic": ['/steer_angle'],
+    "wheelspeed_b_topic": ['/wheelspeed_b'],
+    "wheelspeed_f_topic": ['/wheelspeed_f'],
+    "velodyne_topics": ['/velodyne_nodelet_manager_driver/parameter_descriptions', '/velodyne_nodelet_manager_driver/parameter_updates', '/velodyne_packets'],
+    "points_raw_topic": ['/points_raw']
+}
 # Timeout to wait for a message in seconds
-timeout = 2.0
+timeout = 1.0
 
-def check_topic(topic):
+
+def check_topic_exists(topic):
+    topic_list = rospy.get_published_topics()
+    for t, _ in topic_list:
+        if t == topic:
+            return True
+    return False
+
+
+def check_topic_message(topic):
     try:
         rospy.wait_for_message(topic, rospy.AnyMsg, timeout)
         return True
     except rospy.ROSException:
         return False
 
-def create_label(root, text, is_on):
-    label_color = "green" if is_on else "red"
-    label_text = "{}: {}".format(text, "ON" if is_on else "OFF")
-    tk.Label(root, text=label_text, fg=label_color).pack()
 
-def check_topics_and_create_labels(root, topic_group_name, topics):
-    is_group_on = all(check_topic(topic) for topic in topics)
-    create_label(root, topic_group_name, is_group_on)
+def create_label(root, row, text, column):
+    label_text = text
+    status_text = "OFF"
+    label = tk.Label(root, text=label_text, font=("Helvetica", 10))
+    status = tk.Label(root, text=status_text, font=("Helvetica", 10), fg="red")
+    label.grid(row=row, column=column*2, padx=10, pady=10)  # note the padding
+    status.grid(row=row, column=column*2+1, padx=10, pady=10)  # note the padding
+    return status  # return the status label so we can update it later
+
+
+def check_topics_and_create_labels(root):
+    labels = []  # a list to store the labels
+    row = 0
+    for group_name, topics in topic_groups.items():
+        label_online = create_label(root, row, group_name + " online", 0)
+        label_message = create_label(root, row, group_name + " receiving messages", 1)
+        labels.append((label_online, label_message, topics))  # store the labels and topics
+        row += 1
+    return labels
+
+
+def update_labels(root, labels):
+    for label_online, label_message, topics in labels:
+        is_group_online = all(check_topic_exists(topic) for topic in topics)
+        is_group_on = all(check_topic_message(topic) for topic in topics)
+
+        label_online.config(
+            text="{}".format("ON" if is_group_online else "OFF"),
+            fg="green" if is_group_online else "red")
+        label_message.config(
+            text="{}".format("ON" if is_group_on else "OFF"),
+            fg="green" if is_group_on else "red")
+
+    # Schedule the next update
+    root.after(1000, update_labels, root, labels)  # update every 2 seconds (2000 milliseconds)
+
 
 def main():
     rospy.init_node('topic_checker')
-    root = tk.Tk()
 
-    check_topics_and_create_labels(root, "SBG topics", sbg_topics)
-    check_topics_and_create_labels(root, "IMU topics", imu_topics)
-    check_topics_and_create_labels(root, "Camera 0 topics", camera0_topics)
-    check_topics_and_create_labels(root, "Camera 1 topics", camera1_topics)
-    check_topics_and_create_labels(root, "Camera 2 topics", camera2_topics)
-    check_topics_and_create_labels(root, "Steer Angle topic", steer_angle_topic)
-    check_topics_and_create_labels(root, "Wheelspeed B topic", wheelspeed_b_topic)
-    check_topics_and_create_labels(root, "Wheelspeed F topic", wheelspeed_f_topic)
-    check_topics_and_create_labels(root, "Velodyne topics", velodyne_topics)
-    check_topics_and_create_labels(root, "Points Raw topic", points_raw_topic)
+    root = tk.Tk()
+    root.title("Topic Checker")
+
+    labels = check_topics_and_create_labels(root)
+
+    update_labels(root, labels)  # initial update
 
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
